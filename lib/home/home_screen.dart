c18424ea3bc4f6f86e5dbccde48e12e8df952465
emido <emido886@gmail.com>
2024-12-14 06:58:36 +0200

    initial project Added empty home screen

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,7 @@
+class HomeScreen extends StatelessWidget {
+  static const String routName = 'home_screen' ;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
